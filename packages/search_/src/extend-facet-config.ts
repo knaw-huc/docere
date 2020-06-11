f21b836e5d32acd4e17fb8f6d628c1f559768780
Gijsjan <agijsbro@gmail.com>
2020-06-11 10:09:38 +0200

    Extend facet config for Date facet

--- packages/search_/src/extend-facet-config.ts
+++ packages/search_/src/extend-facet-config.ts
@@ -1,4 +1,4 @@
-import { SortBy, SortDirection, EsDataType, RangeFacetConfig } from '@docere/common'
+import { SortBy, SortDirection, EsDataType, RangeFacetConfig, DateFacetConfig } from '@docere/common'
 
 import { isListFacetConfig, isBooleanFacetConfig, isHierarchyFacetConfig, isRangeFacetConfig, isDateFacetConfig } from './utils'
 
@@ -39,12 +39,19 @@ function extendRangeFacet(config: RangeFacetConfig): RangeFacetConfig {
 	}
 }
 
+function extendDateFacet(config: DateFacetConfig): DateFacetConfig {
+	return {
+		collapseFilters: true,
+		...config,
+	}
+}
+
 function initFacet(facetConfig: FacetConfig): FacetConfig {
 	if		(isListFacetConfig(facetConfig))		return extendListFacet(facetConfig)
 	else if (isBooleanFacetConfig(facetConfig))		return extendBooleanFacet(facetConfig)
 	else if (isHierarchyFacetConfig(facetConfig))	return extendHierarchyFacet(facetConfig)
 	else if (isRangeFacetConfig(facetConfig))		return extendRangeFacet(facetConfig)
-	else if (isDateFacetConfig(facetConfig))		return facetConfig
+	else if (isDateFacetConfig(facetConfig))		return extendDateFacet(facetConfig)
 
 	console.error(`Facet config with datatype: '${facetConfig.datatype}' not found!`)
 }
