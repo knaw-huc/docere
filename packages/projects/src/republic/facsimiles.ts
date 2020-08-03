285b169e59455995b6eaa89a5685c4a350f1fc7e
Gijsjan <agijsbro@gmail.com>
2020-08-03 14:50:50 +0200

    Remove obsolete function from Republic facsimile extraction

--- packages/projects/src/republic/facsimiles.ts
+++ packages/projects/src/republic/facsimiles.ts
@@ -1,62 +1,8 @@
-import { AsideTab, Entry } from '@docere/common'
-import type { FacsimileArea, DocereConfig, Facsimile } from '@docere/common'
+import { Entry } from '@docere/common'
+import type { Facsimile } from '@docere/common'
 
-function parseArea(id: string)  {
-	return id
-		.split('_')
-		.map(x => parseInt(x, 10))
-}
-
-function elementToArea(el: Element): FacsimileArea {
-	const id = el.getAttribute('id')
-	const area = el.getAttribute('area')
-	const [x, y, w, h] = parseArea(area)
-	return { id, x, y, w, h }
-}
-
-function extractFacsimileAreas(doc: XMLDocument, config: DocereConfig) {
-	const areas: FacsimileArea[] = []
-
-	for (const el of doc.querySelectorAll('string')) {
-		const area = elementToArea(el)
-		area.showOnHover = false
-		area.note = { ocr: el.textContent }
-		if (el.hasAttribute('suggestion')) area.note.suggestion = el.getAttribute('suggestion')
-
-		if (el.hasAttribute('ref')) {
-			const type = el.getAttribute('type').split(' ')[0]
-			const textDataConfig = config.entities.find(td => td.id === type)
-			if (textDataConfig == null) console.log(`[extractFacsimileAreas] No config found for: ${type}`)
-
-			area.target = {
-				color: textDataConfig?.color,
-				id: el.getAttribute('ref'),
-				listId: type,
-				asideTab: AsideTab.TextData,
-			}
-		}
-
-		areas.push(area)
-	}
-
-	for (const el of doc.querySelectorAll('block')) {
-		const area = elementToArea(el)
-		area.showOnHover = false
-		areas.push(area)
-	}
-
-	for (const el of doc.querySelectorAll('line')) {
-		const area = elementToArea(el)
-		area.note = { ocr: el.textContent }
-		areas.push(area)
-	}
-
-	return areas
-}
-
-export default function extractFacsimiles(entry: Entry, config: DocereConfig) {
+export default function extractFacsimiles(entry: Entry) {
 	const facsimiles: Facsimile[] = []
-	// const pb = entry.document.querySelector('pb')
 
 	const id = `https://images.diginfra.net/iiif/${entry.id.replace(/\.page$/, '')}`
 
