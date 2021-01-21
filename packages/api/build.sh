acc5b402fbc21b2ad3151b94fd6252dc73928a5d
Gijsjan <agijsbro@gmail.com>
2021-01-21 11:58:03 +0100

    Make sure tsconfigs have an outDir to prevent d.ts files in src/

--- packages/api/build.sh
+++ packages/api/build.sh
@@ -3,5 +3,5 @@
 rm -rf build
 ../../node_modules/.bin/tsc
 
-rm -rf build.puppenv.utils
+rm -rf build.puppenv.data
 DOCERE_DTAP=Development ../../node_modules/.bin/webpack
--- packages/search/build.sh
+++ packages/search/build.sh
@@ -3,7 +3,7 @@
 rm -rf build
 
 # Dev version
-../../node_modules/.bin/webpack
+npx webpack
 
 # Emit declaration files
 npx tsc --declaration --emitDeclarationOnly --outDir build/types
